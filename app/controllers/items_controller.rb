24afe62d6655c6dc2ca65500a0cf234b64fc97c3
KavyaSek <ksekar@chicagobooth.edu>
2024-11-06 23:56:29 +0000

    final

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,25 @@
 class ItemsController < ApplicationController
   def index
     @list_of_items = Item.all
-
     render({ :template => "item_templates/list" })
   end
+
+  def form
+    render({ :template => "item_templates/form" })
+  end
+
+  def update
+    @url = params[:query_link_url]
+    @desc = params[:query_link_description]
+    @img = params[:query_thumbnail_url]
+
+    x = Item.new
+    x.link_url = @url
+    x.link_description = @desc
+    x.thumbnail_url = @img
+    x.save
+
+    redirect_to("/")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
+  get("/backdoor", { :controller => "items", :action => "form" })
+  post("/insert_item", { :controller => "items", :action => "update" })
+
 end
